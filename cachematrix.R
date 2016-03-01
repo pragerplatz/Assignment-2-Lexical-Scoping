8b3ee38c95051a6b2ec599cae45292f54cf9ea9c
pragerplatz <johannes.jacob@outlook.com>
2016-03-01 17:32:33 +0100

    Added files via upload

---
+++ cachematrix.R
@@ -0,0 +1,32 @@
+## Writing a pair of functions that cache the inverse of a matrix
+
+## Creating a matrix which can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) i <<- solve
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+}
+
+
+## Calculating the inverse or taking it from cache
+
+cacheSolve <- function(x, ...) {
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
+}
